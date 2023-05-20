1e6ee60177af9e4180738b69361a005d53b99c0b
Robert Vari <mail.robertvari@gmail.com>
2023-05-20 14:25:38 +0200

    update

---
+++ 03_multiple_positional_arguments.py
@@ -0,0 +1,8 @@
+def print_user_data(name, address, email):
+    print(f"-"*20)
+    print(f"Name: {name}")
+    print(f"Address: {address}")
+    print(f"Email: {email}")
+    print(f"-"*20)
+
+print_user_data("csaba@gmail.com", "Csaba", "Pécs")
