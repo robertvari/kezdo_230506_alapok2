96544d33ee8c7ea84f9227259b2387e65aaa78a1
Robert Vari <mail.robertvari@gmail.com>
2023-05-20 14:32:31 +0200

    update

---
+++ 04_keyword_arguments.py
@@ -0,0 +1,8 @@
+def print_user_data(email, address, name):
+    print(f"-"*20)
+    print(f"Name: {name}")
+    print(f"Address: {address}")
+    print(f"Email: {email}")
+    print(f"-"*20)
+
+print_user_data(name="Csaba", address="Pécs", email="csaba@gmail.com")
