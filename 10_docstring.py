0882e23be48e6cfaa03046e8c198b0003f7acf22
Robert Vari <mail.robertvari@gmail.com>
2023-05-20 15:08:38 +0200

    update

---
+++ 10_docstring.py
@@ -0,0 +1,15 @@
+def print_user_data(name: str, address: str, email:str, age:int):
+    """
+    This function prints the user data in a readable format.
+    name = str
+    address = str
+    email = str
+    age = int
+    """
+
+    print(f"-"*20)
+    print(f"Name: {name}")
+    print(f"Address: {address}")
+    print(f"Email: {email}")
+    print(f"Age: {age}")
+    print(f"-"*20)
