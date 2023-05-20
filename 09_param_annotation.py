d344cba22f410b2ca590850a18425b4f35caf3ef
Robert Vari <mail.robertvari@gmail.com>
2023-05-20 15:02:37 +0200

    update

---
+++ 09_param_annotation.py
@@ -0,0 +1,9 @@
+def print_user_data(name: str, address: str, email:str, age:int):
+    print(f"-"*20)
+    print(f"Name: {name}")
+    print(f"Address: {address}")
+    print(f"Email: {email}")
+    print(f"Age: {age}")
+    print(f"-"*20)
+
+print_user_data(10, "Budapest", "csaba@gmail.com", "32")
