6c92d16879715b9e5ca2234fad09090c2f79b2f8
Robert Vari <mail.robertvari@gmail.com>
2023-05-20 17:02:51 +0200

    update

---
+++ magic_number.py
@@ -0,0 +1,35 @@
+def main():
+    pass
+
+def intro():
+    """
+    Prints the game title and its rules
+    """
+    pass
+
+def get_player_name():
+    pass
+
+def game_loop():
+    """
+    The main game where we ask the player
+    """
+    pass
+
+def get_result():
+    """
+    Check wining conditions
+    """
+    pass
+
+def restart_game():
+    """
+    Ask player if he/she wants to continue the game.
+    """
+    pass
+
+def exit_game():
+    """
+    Exit game
+    """
+    pass
