3a857f467cff7fbea7bc1a2cdd3be846cae10a24
Robert Vari <mail.robertvari@gmail.com>
2023-05-20 16:18:01 +0200

    update

---
+++ 17_sort_dictionary.py
@@ -0,0 +1,9 @@
+movie_list = {
+    1: {"name": "The Princess", "rating": 26},
+    2: {"name": "Spider-Man: No Way Home", "rating": 81},
+    3: {"name": "Morbius", "rating": 64},
+    4: {"name": "The Northman", "rating": 72},
+    5: {"name": "Troll", "rating": 67},
+    6: {"name": "Avatar", "rating": 76},
+    7: {"name": "The Woman King", "rating": 79}
+}
