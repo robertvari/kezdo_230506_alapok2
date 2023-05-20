6c070c80354e8dd0a08c9b061e9f68268b790c2d
Robert Vari <mail.robertvari@gmail.com>
2023-05-20 16:25:26 +0200

    update

--- 17_sort_dictionary.py
+++ 17_sort_dictionary.py
@@ -1,9 +1,13 @@
 movie_list = {
-    1: {"name": "The Princess", "rating": 26},
-    2: {"name": "Spider-Man: No Way Home", "rating": 81},
-    3: {"name": "Morbius", "rating": 64},
-    4: {"name": "The Northman", "rating": 72},
-    5: {"name": "Troll", "rating": 67},
-    6: {"name": "Avatar", "rating": 76},
-    7: {"name": "The Woman King", "rating": 79}
+    1: {"title": "The Princess", "rating": 26},
+    2: {"title": "Spider-Man: No Way Home", "rating": 81},
+    3: {"title": "Morbius", "rating": 64},
+    4: {"title": "The Northman", "rating": 72},
+    5: {"title": "Troll", "rating": 67},
+    6: {"title": "Avatar", "rating": 76},
+    7: {"title": "The Woman King", "rating": 79}
 }
+
+sorted_by_rating = sorted(movie_list, key=lambda movie_id: movie_list[movie_id]["rating"], reverse=True)
+for movie_id in sorted_by_rating:
+    print(movie_list[movie_id])
